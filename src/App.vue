e0870480ea9fbed347fa2ce3e780968f75cc9453
osboxes <miguelangel.quispe@bluetab.net>
2019-11-24 07:24:14 -0500

    commit all:q :

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div style="height:100vh"> 
+	<l-map
+		style="height: 100%"
+		:zoom="zoom"
+		:center="center"
+	>
+
+		<l-tile-layer :url="url"
+			:id="id"
+		>
+		</l-tile-layer>
+		<l-marker :lat-lng="plaza_libertad">
+			<l-icon icon-url="./tuscany_color.png"
+				:icon-size="dynamicSize"
+			>
+			</l-icon>
+	<!--			<l-popup>
+					<div @click="innerClick">
+					I am a popup
+					</div>
+				</l-popup>
+	-->
+			<l-tooltip :options="{ permanent: true, interactive: true }">
+				<div @click="innerClick">
+					Plaza Libertad 
+				</div>
+			</l-tooltip>
+		</l-marker>
+		<l-marker :lat-lng="azampata">
+                        <l-icon icon-url="./Ruin.png"
+                                :icon-size="dynamicSize"
+                        >
+                        </l-icon>
+                        <l-tooltip :options="{ permanent: true, interactive: true }">
+                                <div @click="innerClick">
+                                        azampata
+                                </div>
+                        </l-tooltip>
+                </l-marker>
+
+		<l-marker :lat-lng="quiulacocha">
+                        <l-icon icon-url="./lake.png"
+                                :icon-size="dynamicSize"
+                        >
+			</l-icon>
+                        <l-tooltip :options="{ permanent: true, interactive: true }">
+                                <div @click="innerClick">
+					quiulacocha
+                                </div>
+                        </l-tooltip>
+                </l-marker>
+		<l-marker :lat-lng="casaracra"> 
+                        <l-icon icon-url="./Ruin.png" 
+                                :icon-size="dynamicSize" 
+                        > 
+                        </l-icon> 
+                        <l-tooltip :options="{ permanent: true, interactive: true }"> 
+                                <div @click="innerClick"> 
+					Atacayan
+                                </div> 
+                        </l-tooltip> 
+                </l-marker>	</l-map>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { latLng} from "leaflet";
+import { LMap, LTileLayer,LMarker,LIcon,/*LPopup*/LTooltip } from "vue2-leaflet";
 
 export default {
-  name: 'app',
+  name: "Example",
   components: {
-    HelloWorld
-  }
-}
+	LMap,
+	LTileLayer,
+	LMarker,
+	LIcon/*,
+	LPopup*/,
+	LTooltip
+  },
+  data() {
+    return {
+	zoom:15,
+	center:   latLng(-11.516450, -75.900012),
+	url: "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhY2hvbWlndWVsIiwiYSI6ImNqNXh5d3p1dzA5cWcyd2xoYXVibnpwdDcifQ.SL1EcjSYuczLSynh9qkPDA",
+//	url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+	id:'mapbox.streets',
+	plaza_libertad: latLng(-11.516450, -75.900012),
+	azampata: latLng( -11.525603, -75.900040),
+	quiulacocha: latLng(-10.699309, -76.279053),
+	casaracra: latLng(-11.458187, -75.955077),
+	iconSize:32
+    }
+},
+  computed: {
+    dynamicSize () {
+	console.log(this.iconSize)
+	return [this.iconSize,this.iconSize*1.15];
+    },
+    dynamicAnchor () {
+      return [this.iconSize / 2, this.iconSize * 1.15];
+    }
+  },
+	methods:{
+		innerClick(){
+			console.log("meotods");
+			alert("click!");
+		}
+	}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
