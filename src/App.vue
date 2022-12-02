3fd140133ec4c1081d5132a347c314fe65b0d26f
Miguel Angel <maccgeo@hotmail.com>
2022-12-02 14:02:59 +0100

    prueba de concepto funcional con mapbox-gl

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,29 @@
 <template>
- <div style="height:100vh"> 
-	<l-map
-		style="height: 100%"
-		:zoom="zoom"
-		:center="center"
-	>
-
-		<l-tile-layer :url="url"
-			:id="id"
-		>
-		</l-tile-layer>
-		<l-marker :lat-lng="plaza_libertad">
-			<l-icon icon-url="./tuscany_color.png"
-				:icon-size="dynamicSize"
-			>
-			</l-icon>
-	<!--			<l-popup>
-					<div @click="innerClick">
-					I am a popup
-					</div>
-				</l-popup>
-	-->
-			<l-tooltip :options="{ permanent: true, interactive: true }">
-				<div @click="innerClick">
-					Plaza Libertad 
-				</div>
-			</l-tooltip>
-		</l-marker>
-		<l-marker :lat-lng="azampata">
-                        <l-icon icon-url="./Ruin.png"
-                                :icon-size="dynamicSize"
-                        >
-                        </l-icon>
-                        <l-tooltip :options="{ permanent: true, interactive: true }">
-                                <div @click="innerClick">
-                                        azampata
-                                </div>
-                        </l-tooltip>
-                </l-marker>
-
-		<l-marker :lat-lng="quiulacocha">
-                        <l-icon icon-url="./lake.png"
-                                :icon-size="dynamicSize"
-                        >
-			</l-icon>
-                        <l-tooltip :options="{ permanent: true, interactive: true }">
-                                <div @click="innerClick">
-					quiulacocha
-                                </div>
-                        </l-tooltip>
-                </l-marker>
-		<l-marker :lat-lng="casaracra"> 
-                        <l-icon icon-url="./Ruin.png" 
-                                :icon-size="dynamicSize" 
-                        > 
-                        </l-icon> 
-                        <l-tooltip :options="{ permanent: true, interactive: true }"> 
-                                <div @click="innerClick"> 
-					Atacayan
-                                </div> 
-                        </l-tooltip> 
-                </l-marker>	</l-map>
+ <div style="height:100vh" id="map"> 
+	
  </div>
 </template>
 
 <script>
-import { latLng} from "leaflet";
-import { LMap, LTileLayer,LMarker,LIcon,/*LPopup*/LTooltip } from "vue2-leaflet";
+import mapboxgl from 'mapbox-gl'; 
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default {
   name: "Example",
   components: {
-	LMap,
-	LTileLayer,
-	LMarker,
-	LIcon/*,
-	LPopup*/,
-	LTooltip
+
   },
   data() {
     return {
-	zoom:15,
-	center:   latLng(-11.516450, -75.900012),
-	url: "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hhY2hvbWlndWVsIiwiYSI6ImNqNXh5d3p1dzA5cWcyd2xoYXVibnpwdDcifQ.SL1EcjSYuczLSynh9qkPDA",
-//	url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+		map:{}
+	/*zoom:15,
+	center:   latLng(-11.516450, -75.900012),	
 	id:'mapbox.streets',
 	plaza_libertad: latLng(-11.516450, -75.900012),
 	azampata: latLng( -11.525603, -75.900040),
 	quiulacocha: latLng(-10.699309, -76.279053),
 	casaracra: latLng(-11.458187, -75.955077),
-	iconSize:32
+	iconSize:32*/
     }
 },
   computed: {
@@ -101,10 +35,25 @@ export default {
       return [this.iconSize / 2, this.iconSize * 1.15];
     }
   },
+  mounted:function(){
+	this.crear_mapa()
+	this.add_marker()
+  },
 	methods:{
-		innerClick(){
-			console.log("meotods");
-			alert("click!");
+		crear_mapa:function(){
+			mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhY2hvbWlndWVsIiwiYSI6ImNqNXh5d3p1dzA5cWcyd2xoYXVibnpwdDcifQ.SL1EcjSYuczLSynh9qkPDA';
+			this.map = new mapboxgl.Map({
+			container: 'map', // container ID
+			style: 'mapbox://styles/mapbox/streets-v12', // style URL
+			center: [-77.04221094667754,-12.040683822019647], // starting position [lng, lat]			
+			zoom: 9, // starting zoom
+			});
+			//console.log(map)
+		},
+		add_marker:function(){
+			new mapboxgl.Marker()
+			.setLngLat([-77.04221094667754,-12.040683822019647])
+			.addTo(this.map);
 		}
 	}
 };
--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@ import App from './App.vue'
 import './quasar'
 
 Vue.config.productionTip = false
-import 'leaflet/dist/leaflet.css'
+//import 'leaflet/dist/leaflet.css'
 
-import { Icon }  from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+//import { Icon }  from 'leaflet'
+//import 'leaflet/dist/leaflet.css'
 
 
 // this part resolve an issue where the markers would not appear
-delete Icon.Default.prototype._getIconUrl;
+//delete Icon.Default.prototype._getIconUrl;
 
-Icon.Default.mergeOptions({
+/*Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
+});*/
 
 new Vue({
   render: h => h(App),
